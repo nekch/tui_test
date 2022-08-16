c998161097255291871faafe05cf49328e36f14f
Nikita Nizhehorodov <nikita.nizhe@gmail.com>
2022-08-16 13:27:44 +0200

    Add minor changes to tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -11,19 +11,6 @@ import { UserRepositoriesInfo } from '../src/user-repositories/test/mocks';
 describe('e2e', () => {
   let app: INestApplication;
 
-  const incorrectHeader = 'text/plain';
-  const repoName = UserRepositoriesInfo[0].name;
-  const { name: branchName, commitSha: sha } = UserRepositoriesInfo[0].branches[0];
-
-  nock(Mocks.GIT_BASE_URL)
-    .get('/users/nekch')
-    .reply(200, { body: UserRepositoriesInfo })
-    .get('/users/nekch/repos')
-    .query({ per_page: UserRepositories.PER_PAGE, page: 1 })
-    .reply(200, [{ name: repoName, fork: false }])
-    .get('/repos/nekch/tui_test/branches')
-    .reply(200, [{ name: branchName, commit: { sha } }]);
-
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -34,6 +21,19 @@ describe('e2e', () => {
   });
 
   describe('/user-repositories (GET)', () => {
+    const incorrectHeader = 'text/plain';
+    const repoName = UserRepositoriesInfo[0].name;
+    const { name: branchName, commitSha: sha } = UserRepositoriesInfo[0].branches[0];
+
+    nock(Mocks.GIT_BASE_URL)
+      .get('/users/nekch')
+      .reply(200, { body: UserRepositoriesInfo })
+      .get('/users/nekch/repos')
+      .query({ per_page: UserRepositories.PER_PAGE, page: 1 })
+      .reply(200, [{ name: repoName, fork: false }])
+      .get('/repos/nekch/tui_test/branches')
+      .reply(200, [{ name: branchName, commit: { sha } }]);
+
     it('should return user repositories info', async () => {
       const { body } = await request(app.getHttpServer())
         .get('/user-repositories')
